1f8f5c49bf1f7e6a20ba97dec199c74a4d149839
Felipe Viana <felipetiburcioviana@gmail.com>
2026-09-02 11:50:06 -0400

    Run static code analysis

--- server.py
+++ server.py
@@ -14,14 +14,20 @@ app = Flask("Sentiment Analyzer")
 def sent_analyzer():
     # Retrieve the text to analyze from the request arguments
     text_to_analyze = request.args.get('textToAnalyze')
+
     # Pass the text to the sentiment_analyzer function and store the response
     response = sentiment_analyzer(text_to_analyze)
+
     # Extract the label and score from the response
-   if label is None:
+    label = response['label']
+    score = response['score']
+
+    # Check if the label is None, indicating an error or invalid input
+    if label is None:
         return "Invalid input! Try again."
-    else:
-        # Return a formatted string with the sentiment label and score
-        return "The given text has been identified as {} with a score of {}.".format(label.split('_')[1], score)
+    
+    return "The given text has been identified as {} with a score of {}.".format(label.split('_')[1], score)
+
 
 @app.route("/")
 def render_index_page():
